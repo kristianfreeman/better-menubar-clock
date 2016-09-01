0d650ca6624de400f9f6d406452ade5b9b5e1803
Kristian Freeman <kristian@simple.com>
2016-09-01 11:23:55 -0400

    Packaging, quitting

--- main.js
+++ main.js
@@ -5,6 +5,10 @@ const moment = require('moment')
 
 var appTray, contextMenu
 
+var menuTemplate = [
+  { label: 'Quit', click: app.quit }
+]
+
 updateClock = function(tray) {
   tray.setTitle(moment().format('Y-MM-DD hh:mm'))
   _.delay(updateClock, 1000, tray)
@@ -12,7 +16,11 @@ updateClock = function(tray) {
 
 app.on('ready', function(){
   const icon = Electron.nativeImage.createEmpty()
+  contextMenu = Menu.buildFromTemplate(menuTemplate);
   appTray = new Tray(icon)
+
+  appTray.setContextMenu(contextMenu);
   updateClock(appTray)
 })
 
+// app.dock.hide()
