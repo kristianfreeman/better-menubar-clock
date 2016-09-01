3a9c120f35dfae58d603eaa48a1dad74aef13a61
Kristian Freeman <kristian@simple.com>
2016-09-01 15:59:19 -0400

    Preferences

--- main.js
+++ main.js
@@ -1,26 +1,56 @@
 const Electron = require('electron')
-const {app, Tray, Menu, BrowserWindow} = require('electron')
+const {app, Tray, Menu, BrowserWindow, ipcMain} = require('electron')
 const _ = require('lodash')
 const moment = require('moment')
+const storage = require('electron-json-storage');
 
-var appTray, contextMenu
+var appTray, contextMenu, browserWindow, retrievedDate
 
-var menuTemplate = [
-  { label: 'Quit', click: app.quit }
-]
+getDateFormat = function() {
+  storage.get('dateFormat', function(error, data) {
+    if (error) throw error;
+    retrievedDate = data
+  })
+}
+getDateFormat()
 
 updateClock = function(tray) {
-  tray.setTitle(moment().format('Y-MM-DD hh:mm'))
+  tray.setTitle(moment().format(retrievedDate))
   _.delay(updateClock, 1000, tray)
 }
 
 app.on('ready', function(){
+  browserWindow = new BrowserWindow({
+    show: false,
+    height: 300,
+    width: 400
+  })
+
+  browserWindow.loadURL(`file://${__dirname}/static/preferences.html`)
+
+  var menuTemplate = [
+    { label: 'Preferences', click: () => { browserWindow.show() }},
+    { label: 'Quit', click: app.quit }
+  ]
+
   const icon = Electron.nativeImage.createEmpty()
   contextMenu = Menu.buildFromTemplate(menuTemplate);
   appTray = new Tray(icon)
 
   appTray.setContextMenu(contextMenu);
+
+  ipcMain.on('sendDateFormat', (event, arg) => {
+    storage.set('dateFormat', arg, function(error) {
+      if (error) throw error;
+      getDateFormat()
+    })
+  })
+
   updateClock(appTray)
+
+  browserWindow.webContents.on('did-finish-load', function() {
+    browserWindow.webContents.send('receiveDateFormat', retrievedDate)
+  })
 })
 
 app.dock.hide()
